b32e9543bf4dd685dd7a35e7951d60998b1ab0c7
duravkin <aduravkin04@gmail.com>
2025-11-29 17:44:50 +0300

    Улучшение сборки контейнеров.

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -22,7 +22,7 @@ func Run() {
 		" password=" + os.Getenv("DB_PASSWORD") +
 		" dbname=" + os.Getenv("DB_NAME") +
 		" port=" + os.Getenv("DB_PORT") +
-		" sslmode=disable TimeZone=Europe/Moscow"
+		" sslmode=disable" // Здесь была тайм зона, но та вызывала ошибки.
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
